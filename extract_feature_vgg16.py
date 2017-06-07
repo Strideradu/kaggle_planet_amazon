deaa67d4c237d90713dd5e293e0cafa232254ab6
Strideradu <dunan00001@hotmail.com>
2017-06-07 14:13:48 -0400

    convert train jpg to tf record

--- extract_feature_vgg16.py
+++ extract_feature_vgg16.py
@@ -15,34 +15,10 @@ def _bytes_feature(value):
 def _int64_feature(value):
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
-# use vgg 16 model extract feature
-
-base_model = VGG16(weights='imagenet', pooling = max)
-model = Model(inputs=base_model.input, outputs=base_model.get_layer('fc1').output)
-
-img_path = "/mnt/home/dunan/Learn/Kaggle/planet_amazon/train-jpg/train_9.jpg"
-img = image.load_img(img_path, target_size=(224, 224))
-x = image.img_to_array(img)
-x = np.expand_dims(x, axis=0)
-x = preprocess_input(x)
-
-
-features = model.predict(x)
-print(features)
-
-img_path = "/mnt/home/dunan/Learn/Kaggle/planet_amazon/train-jpg/train_8.jpg"
-img = image.load_img(img_path, target_size=(224, 224))
-x = image.img_to_array(img)
-x = np.expand_dims(x, axis=0)
-x = preprocess_input(x)
-
-
-features = model.predict(x)
-print(features)
-
-# add label
+train_path = "/mnt/home/dunan/Learn/Kaggle/planet_amazon/train-jpg/"
+test_path = "/mnt/home/dunan/Learn/Kaggle/planet_amazon/test-jpg/"
 train = pd.read_csv("/mnt/home/dunan/Learn/Kaggle/planet_amazon/train_v2.csv")
-y_train = []
+test = pd.read_csv("/mnt/home/dunan/Learn/Kaggle/planet_amazon/sample_submission_v2.csv")
 
 flatten = lambda l: [item for sublist in l for item in sublist]
 labels = list(set(flatten([l.split(' ') for l in train['tags'].values])))
@@ -50,17 +26,39 @@ labels = list(set(flatten([l.split(' ') for l in train['tags'].values])))
 label_map = {l: i for i, l in enumerate(labels)}
 inv_label_map = {i: l for l, i in label_map.items()}
 
-for tags in tqdm(train.tags.values, miniters=50):
+# use vgg 16 model extract feature from fc1 layer
+base_model = VGG16(weights='imagenet', pooling = max)
+model = Model(inputs=base_model.input, outputs=base_model.get_layer('fc1').output)
+
+tfrecords_filename = "/mnt/home/dunan/Learn/Kaggle/planet_amazon/extracted_feature/vgg16_fc1_train.tfrecord"
+writer = tf.python_io.TFRecordWriter(tfrecords_filename)
+for f, tags in tqdm(train.values[:1000], miniters=1000):
+    # preprocess input image
+    img_path = train_path + "{}.jpg".format(f)
+    img = image.load_img(img_path, target_size=(224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+
+    # generate feature [4096]
+    features = model.predict(x)
+
+    # generate one hot vecctor for label
     targets = np.zeros(17)
     for t in tags.split(' '):
         targets[label_map[t]] = 1
-    y_train.append(targets)
 
+    # generate file id by replace test to 100000 and file to 200000
+    if f.split("_")[0] == "test":
+        file_id = int(f.split("_")[-1]) + 1000000
+    else:
+        file_id= int(f.split("_")[-1]) + 2000000
 
-print(len(y_train))
+    example = tf.train.Example(features=tf.train.Features(feature={
+        'video_id': _int64_feature(file_id),
+        'labels': _int64_feature(targets),
+        'rgb': _bytes_feature(features)}))
 
+    writer.write(example.SerializeToString())
 
-# save the feature to the tf record file
-
-tfrecords_filename = 'pascal_voc_segmentation.tfrecords'
-writer = tf.python_io.TFRecordWriter(tfrecords_filename)
+writer.close()
