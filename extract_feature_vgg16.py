75726ca69d7ce93eb14e3542169c2116a9f189c3
Strideradu <dunan00001@hotmail.com>
2017-06-07 15:21:10 -0400

    Add process test

--- extract_feature_vgg16.py
+++ extract_feature_vgg16.py
@@ -37,7 +37,7 @@ model = Model(inputs=base_model.input, outputs=base_model.get_layer('fc1').outpu
 
 tfrecords_filename = "/mnt/home/dunan/Learn/Kaggle/planet_amazon/extracted_feature/vgg16_fc1_train.tfrecord"
 writer = tf.python_io.TFRecordWriter(tfrecords_filename)
-for f, tags in tqdm(train.values[:1000], miniters=1000):
+for f, tags in tqdm(train.values[:], miniters=1000):
     # preprocess input image
     img_path = train_path + "{}.jpg".format(f)
     img = image.load_img(img_path, target_size=(224, 224))
@@ -63,6 +63,36 @@ for f, tags in tqdm(train.values[:1000], miniters=1000):
         file_id = int(f.split("_")[-1]) + 1000000
     else:
         file_id= int(f.split("_")[-1]) + 2000000
+
+    example = tf.train.Example(features=tf.train.Features(feature={
+        'video_id': _bytes_feature(f.encode('utf-8')),
+        'labels': _int64_feature(targets),
+        'rgb': _bytes_feature(features.tobytes())}))
+
+    writer.write(example.SerializeToString())
+
+writer.close()
+
+tfrecords_filename = "/mnt/home/dunan/Learn/Kaggle/planet_amazon/extracted_feature/vgg16_fc1_test.tfrecord"
+writer = tf.python_io.TFRecordWriter(tfrecords_filename)
+for f, tags in tqdm(test.values[:], miniters=1000):
+    # preprocess input image
+    img_path = train_path + "{}.jpg".format(f)
+    img = image.load_img(img_path, target_size=(224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+
+    # generate feature [4096]
+    features = model.predict(x)
+
+    # generate one hot vecctor for label
+    """
+    targets = np.zeros(17)
+    for t in tags.split(' '):
+        targets[label_map[t]] = 1
+    """
+    targets = []
 
     example = tf.train.Example(features=tf.train.Features(feature={
         'video_id': _bytes_feature(f.encode('utf-8')),
