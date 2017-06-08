458578a195c2c90d925124834540f943a32d3b27
Strideradu <dunan00001@hotmail.com>
2017-06-08 13:36:05 -0400

    use features[0]

--- extract_feature_vgg16.py
+++ extract_feature_vgg16.py
@@ -51,8 +51,7 @@ for f, tags in tqdm(train.values[:], miniters=1000):
 
     # generate feature [4096]
     features = model.predict(x)
-    print(features.shape)
-
+    
     # generate one hot vecctor for label
 
     targets = []
@@ -68,7 +67,7 @@ for f, tags in tqdm(train.values[:], miniters=1000):
     example = tf.train.Example(features=tf.train.Features(feature={
         'video_id': _bytes_feature(f.encode('utf-8')),
         'labels': _int64_feature(targets),
-        'rgb': _float_feature(features)}))
+        'rgb': _float_feature(features[0])}))
 
     writer.write(example.SerializeToString())
 
