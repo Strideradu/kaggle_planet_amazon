aac8a0ebebaee35916ba038cbffe28c828fd61bb
Strideradu <dunan00001@hotmail.com>
2017-06-14 15:52:24 -0400

    set shape for flatten

--- resnet50_transfer.py
+++ resnet50_transfer.py
@@ -35,7 +35,7 @@ inv_label_map = {i: l for l, i in label_map.items()}
 # use vgg 16 model extract feature from fc1 layer
 base_model = ResNet50(weights='imagenet', pooling=max, include_top = False)
 x = base_model.output
-x = Flatten()(x)
+x = Flatten(input_shape=base_model.output_shape[1:])(x)
 predictions = Dense(17, activation='softmax', name='fc1000')(x)
 model = Model(inputs=base_model.input, outputs=predictions)
 
