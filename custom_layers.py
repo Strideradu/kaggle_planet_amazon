058601d36e196517b21df391922ffc298d07a7a1
Strideradu <dunan00001@hotmail.com>
2017-06-19 15:47:21 -0400

    add removed get_from_module form keras 1

--- custom_layers.py
+++ custom_layers.py
@@ -2,8 +2,31 @@ from keras import initializers as initializations
 from keras.engine import Layer, InputSpec
 
 import keras.backend as K
+import six
 
-from keras.utils.generic_utils import get_from_module
+def get_from_module(identifier, module_params, module_name,
+                    instantiate=False, kwargs=None):
+    if isinstance(identifier, six.string_types):
+        res = module_params.get(identifier)
+        if not res:
+            raise Exception('Invalid ' + str(module_name) + ': ' +
+                            str(identifier))
+        if instantiate and not kwargs:
+            return res()
+        elif instantiate and kwargs:
+            return res(**kwargs)
+        else:
+            return res
+    elif type(identifier) is dict:
+        name = identifier.pop('name')
+        res = module_params.get(name)
+        if res:
+            return res(**identifier)
+        else:
+            raise Exception('Invalid ' + str(module_name) + ': ' +
+                            str(identifier))
+    return identifier
+
 def get(identifier, **kwargs):
     return get_from_module(identifier, globals(),
                            'initialization', kwargs=kwargs)
