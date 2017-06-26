d60fff295797dcbafbe44df97be6570a2943275c
Nan Du <dunan00001@gmail.com>
2017-06-26 19:13:54 -0400

    remove graphviz

--- net/util.py
+++ net/util.py
@@ -5,7 +5,7 @@
 
 
 from net.common import *
-from graphviz import Digraph
+# from graphviz import Digraph
 from torch.autograd import Variable
 import builtins
 import time
@@ -73,35 +73,6 @@ class Logger(object):
         # this handles the flush command by doing nothing.
         # you might want to specify some extra behavior here.
         pass
-
-
-# net ------------------------------------
-
-## this is broken !!!
-def make_dot(var):
-    node_attr = dict(style='filled',
-                     shape='box',
-                     align='left',
-                     fontsize='12',
-                     ranksep='0.1',
-                     height='0.2')
-    dot = Digraph(node_attr=node_attr, graph_attr=dict(size="12,12"))
-    seen = set()
-
-    def add_nodes(var):
-        if var not in seen:
-            if isinstance(var, Variable):
-                value = '('+(', ').join(['%d'% v for v in var.size()])+')'
-                dot.node(str(id(var)), str(value), fillcolor='lightblue')
-            else:
-                dot.node(str(id(var)), str(type(var).__name__))
-            seen.add(var)
-            if hasattr(var, 'previous_functions'):
-                for u in var.previous_functions:
-                    dot.edge(str(id(u[0])), str(id(var)))
-                    add_nodes(u[0])
-    add_nodes(var.creator)
-    return dot
 
 
 # https://github.com/pytorch/examples/blob/master/imagenet/main.py ###############
