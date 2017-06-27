2d19bbcbad09d7cd74c6a150eacbe9326d19d40a
Nan Du <dunan00001@gmail.com>
2017-06-26 23:58:28 -0400

    TRY AGAIN

--- net/model/pyramidnet.py
+++ net/model/pyramidnet.py
@@ -43,6 +43,14 @@ def make_flat(out):
     return flat
 
 
+def make_list(*args):
+    module_list = nn.ModuleList([])
+    for arg in args:
+        module_list.extend(arg)
+
+    return module_list
+
+
 #############################################################################3
 
 
@@ -52,179 +60,200 @@ class PyNet_10(nn.Module):
         # in_channels, height, width = in_shape
 
         self.preprocess = nn.Sequential(
-            make_conv_bn_relu(3, 16, kernel_size=1, stride=1, padding=0) +
-            make_conv_bn_relu(16, 16, kernel_size=1, stride=1, padding=0) +
-            make_conv_bn_relu(16, 16, kernel_size=1, stride=1, padding=0) +
-            make_conv_bn_relu(16, 16, kernel_size=1, stride=1, padding=0)
+            make_list(
+                make_conv_bn_relu(3, 16, kernel_size=1, stride=1, padding=0),
+                make_conv_bn_relu(16, 16, kernel_size=1, stride=1, padding=0),
+                make_conv_bn_relu(16, 16, kernel_size=1, stride=1, padding=0),
+                make_conv_bn_relu(16, 16, kernel_size=1, stride=1, padding=0))
         )  # 128
 
         self.conv1d = nn.Sequential(
-            make_conv_bn_relu(16, 32, kernel_size=1, stride=1, padding=0) +
-            make_conv_bn_relu(32, 32, kernel_size=3, stride=1, padding=1) +
-            make_conv_bn_relu(32, 64, kernel_size=1, stride=1, padding=0)
+            make_list(
+                make_conv_bn_relu(16, 32, kernel_size=1, stride=1, padding=0),
+                make_conv_bn_relu(32, 32, kernel_size=3, stride=1, padding=1),
+                make_conv_bn_relu(32, 64, kernel_size=1, stride=1, padding=0))
+
         )  # 128
+
         self.shortld = nn.Conv2d(16, 64, kernel_size=1, stride=1, padding=0, bias=False)
 
         self.conv2d = nn.Sequential(
-            make_conv_bn_relu(64, 64, kernel_size=1, stride=1, padding=0) +
-            make_conv_bn_relu(64, 64, kernel_size=3, stride=1, padding=1) +
-            make_conv_bn_relu(64, 128, kernel_size=1, stride=1, padding=0)
+            make_list(
+                make_conv_bn_relu(64, 64, kernel_size=1, stride=1, padding=0),
+                make_conv_bn_relu(64, 64, kernel_size=3, stride=1, padding=1),
+                make_conv_bn_relu(64, 128, kernel_size=1, stride=1, padding=0))
+
         )  # 64
+
+
         self.short2d = nn.Conv2d(64, 128, kernel_size=1, stride=1, padding=0, bias=False)
 
         self.conv3d = nn.Sequential(
-            make_conv_bn_relu(128, 128, kernel_size=1, stride=1, padding=0) +
-            make_conv_bn_relu(128, 128, kernel_size=3, stride=1, padding=1, groups=16) +
-            make_conv_bn_relu(128, 256, kernel_size=1, stride=1, padding=0)
+            make_list(
+                make_conv_bn_relu(128, 128, kernel_size=1, stride=1, padding=0),
+                make_conv_bn_relu(128, 128, kernel_size=3, stride=1, padding=1, groups=16),
+                make_conv_bn_relu(128, 256, kernel_size=1, stride=1, padding=0))
         )  # 32
         self.short3d = nn.Conv2d(128, 256, kernel_size=1, stride=1, padding=0, bias=False)
 
         self.conv4d = nn.Sequential(
-            make_conv_bn_relu(256, 256, kernel_size=1, stride=1, padding=0) +
-            make_conv_bn_relu(256, 256, kernel_size=3, stride=1, padding=1, groups=16) +
-            make_conv_bn_relu(256, 256, kernel_size=1, stride=1, padding=0)
+            make_list(
+                make_conv_bn_relu(256, 256, kernel_size=1, stride=1, padding=0),
+                make_conv_bn_relu(256, 256, kernel_size=3, stride=1, padding=1, groups=16),
+                make_conv_bn_relu(256, 256, kernel_size=1, stride=1, padding=0))
         )  # 16
         self.short4d = None  # nn.Identity()
 
         self.conv5d = nn.Sequential(
-            make_conv_bn_relu(256, 256, kernel_size=1, stride=1, padding=0) +
-            make_conv_bn_relu(256, 256, kernel_size=3, stride=1, padding=1, groups=16) +
-            make_conv_bn_relu(256, 256, kernel_size=1, stride=1, padding=0)
+            make_list(
+                make_conv_bn_relu(256, 256, kernel_size=1, stride=1, padding=0),
+                make_conv_bn_relu(256, 256, kernel_size=3, stride=1, padding=1, groups=16),
+                make_conv_bn_relu(256, 256, kernel_size=1, stride=1, padding=0))
         )  # 8
         self.short5d = None  # nn.Identity()
 
         self.conv4u = nn.Sequential(
-            make_conv_bn_relu(256, 256, kernel_size=1, stride=1, padding=0) +
-            make_conv_bn_relu(256, 256, kernel_size=3, stride=1, padding=1, groups=16) +
-            make_conv_bn_relu(256, 256, kernel_size=1, stride=1, padding=0)
+            make_list(
+                make_conv_bn_relu(256, 256, kernel_size=1, stride=1, padding=0),
+                make_conv_bn_relu(256, 256, kernel_size=3, stride=1, padding=1, groups=16),
+                make_conv_bn_relu(256, 256, kernel_size=1, stride=1, padding=0))
         )  # 16
 
         self.conv3u = nn.Sequential(
-            make_conv_bn_relu(256, 128, kernel_size=1, stride=1, padding=0) +
-            make_conv_bn_relu(128, 128, kernel_size=3, stride=1, padding=1, groups=16) +
-            make_conv_bn_relu(128, 128, kernel_size=1, stride=1, padding=0)
+            make_list(
+                make_conv_bn_relu(256, 128, kernel_size=1, stride=1, padding=0),
+                make_conv_bn_relu(128, 128, kernel_size=3, stride=1, padding=1, groups=16),
+                make_conv_bn_relu(128, 128, kernel_size=1, stride=1, padding=0))
         )  # 32
 
         self.conv2u = nn.Sequential(
-            make_conv_bn_relu(128, 64, kernel_size=1, stride=1, padding=0) +
-            make_conv_bn_relu(64, 64, kernel_size=3, stride=1, padding=1) +
-            make_conv_bn_relu(64, 64, kernel_size=1, stride=1, padding=0)
+            make_list(
+                make_conv_bn_relu(128, 64, kernel_size=1, stride=1, padding=0),
+                make_conv_bn_relu(64, 64, kernel_size=3, stride=1, padding=1),
+                make_conv_bn_relu(64, 64, kernel_size=1, stride=1, padding=0))
         )  # 64
 
         self.conv1u = nn.Sequential(
-            make_conv_bn_relu(64, 64, kernel_size=1, stride=1, padding=0) +
-            make_conv_bn_relu(64, 64, kernel_size=3, stride=1, padding=1) +
-            make_conv_bn_relu(64, 64, kernel_size=1, stride=1, padding=0)
+            make_list(
+                make_conv_bn_relu(64, 64, kernel_size=1, stride=1, padding=0),
+                make_conv_bn_relu(64, 64, kernel_size=3, stride=1, padding=1),
+                make_conv_bn_relu(64, 64, kernel_size=1, stride=1, padding=0))
         )  # 128
 
         self.cls2d = nn.Sequential(
-            make_linear_bn_relu(128, 512) +
-            make_linear_bn_relu(512, 512) +
-            [nn.Linear(512, num_classes)]
+            make_list(
+                make_linear_bn_relu(128, 512),
+                make_linear_bn_relu(512, 512),
+                [nn.Linear(512, num_classes)])
         )
         self.cls3d = nn.Sequential(
-            make_linear_bn_relu(256, 512) +
-            make_linear_bn_relu(512, 512) +
-            [nn.Linear(512, num_classes)]
+            make_list(
+                make_linear_bn_relu(256, 512),
+                make_linear_bn_relu(512, 512),
+                [nn.Linear(512, num_classes)])
         )
         self.cls4d = nn.Sequential(
-            make_linear_bn_relu(256, 512) +
-            make_linear_bn_relu(512, 512) +
-            [nn.Linear(512, num_classes)]
+            make_list(
+                make_linear_bn_relu(256, 512),
+                make_linear_bn_relu(512, 512),
+                [nn.Linear(512, num_classes)])
         )
         self.cls5d = nn.Sequential(
-            make_linear_bn_relu(256, 512) +
-            make_linear_bn_relu(512, 512) +
-            [nn.Linear(512, num_classes)]
+            make_list(
+                make_linear_bn_relu(256, 512),
+                make_linear_bn_relu(512, 512),
+                [nn.Linear(512, num_classes)])
         )
 
         self.cls1u = nn.Sequential(
-            make_linear_bn_relu(64, 512) +
-            make_linear_bn_relu(512, 512) +
-            [nn.Linear(512, num_classes)]
+            make_list(
+                make_linear_bn_relu(64, 512),
+                make_linear_bn_relu(512, 512),
+                [nn.Linear(512, num_classes)])
         )
         self.cls2u = nn.Sequential(
-            make_linear_bn_relu(64, 512) +
-            make_linear_bn_relu(512, 512) +
-            [nn.Linear(512, num_classes)]
+            make_list(
+                make_linear_bn_relu(64, 512),
+                make_linear_bn_relu(512, 512),
+                [nn.Linear(512, num_classes)])
         )
         self.cls3u = nn.Sequential(
-            make_linear_bn_relu(128, 512) +
-            make_linear_bn_relu(512, 512) +
-            [nn.Linear(512, num_classes)]
+            make_list(
+                make_linear_bn_relu(128, 512),
+                make_linear_bn_relu(512, 512),
+                [nn.Linear(512, num_classes)])
         )
         self.cls4u = nn.Sequential(
-            make_linear_bn_relu(256, 512) +
-            make_linear_bn_relu(512, 512) +
-            [nn.Linear(512, num_classes)]
+            make_list(
+                make_linear_bn_relu(256, 512),
+                make_linear_bn_relu(512, 512),
+                [nn.Linear(512, num_classes)])
         )
 
-    def forward(self, x):
-        out = self.preprocess(x)  # 128
 
-        conv1d = self.conv1d(out)  # 128
-        out = F.max_pool2d(conv1d, kernel_size=2, stride=2)  # 64
+def forward(self, x):
+    out = self.preprocess(x)  # 128
 
-        conv2d = self.conv2d(out) + make_shortcut(out, self.short2d)  # 64
-        out = F.max_pool2d(conv2d, kernel_size=2, stride=2)  # 32
-        flat2d = make_max_flat(out)
+    conv1d = self.conv1d(out)  # 128
+    out = F.max_pool2d(conv1d, kernel_size=2, stride=2)  # 64
 
-        conv3d = self.conv3d(out) + make_shortcut(out, self.short3d)  # 32
-        out = F.max_pool2d(conv3d, kernel_size=2, stride=2)  # 16
-        flat3d = make_max_flat(out)
+    conv2d = self.conv2d(out) + make_shortcut(out, self.short2d)  # 64
+    out = F.max_pool2d(conv2d, kernel_size=2, stride=2)  # 32
+    flat2d = make_max_flat(out)
 
-        conv4d = self.conv4d(out) + make_shortcut(out, self.short4d)  # 16
-        out = F.max_pool2d(conv4d, kernel_size=2, stride=2)  # 8
-        flat4d = make_max_flat(out)
+    conv3d = self.conv3d(out) + make_shortcut(out, self.short3d)  # 32
+    out = F.max_pool2d(conv3d, kernel_size=2, stride=2)  # 16
+    flat3d = make_max_flat(out)
 
-        conv5d = self.conv5d(out) + make_shortcut(out, self.short5d)  # 8
-        out = conv5d  # 4
-        flat5d = make_max_flat(out)
+    conv4d = self.conv4d(out) + make_shortcut(out, self.short4d)  # 16
+    out = F.max_pool2d(conv4d, kernel_size=2, stride=2)  # 8
+    flat4d = make_max_flat(out)
 
-        out = F.upsample_bilinear(out, scale_factor=2)  # 16
-        out = out + conv4d
-        out = self.conv4u(out)
-        flat4u = make_max_flat(out)
+    conv5d = self.conv5d(out) + make_shortcut(out, self.short5d)  # 8
+    out = conv5d  # 4
+    flat5d = make_max_flat(out)
 
-        out = F.upsample_bilinear(out, scale_factor=2)  # 32
-        out = out + conv3d
-        out = self.conv3u(out)
-        flat3u = make_max_flat(out)
+    out = F.upsample_bilinear(out, scale_factor=2)  # 16
+    out = out + conv4d
+    out = self.conv4u(out)
+    flat4u = make_max_flat(out)
 
-        out = F.upsample_bilinear(out, scale_factor=2)  # 64
-        out = out + conv2d
-        out = self.conv2u(out)
-        flat2u = make_max_flat(out)
+    out = F.upsample_bilinear(out, scale_factor=2)  # 32
+    out = out + conv3d
+    out = self.conv3u(out)
+    flat3u = make_max_flat(out)
 
-        out = F.upsample_bilinear(out, scale_factor=2)  # 128
-        out = out + conv1d
-        out = self.conv1u(out)
-        flat1u = make_max_flat(out)
+    out = F.upsample_bilinear(out, scale_factor=2)  # 64
+    out = out + conv2d
+    out = self.conv2u(out)
+    flat2u = make_max_flat(out)
 
-        logit2d = self.cls2d(flat2d).unsqueeze(2)
-        logit3d = self.cls3d(flat3d).unsqueeze(2)
-        logit4d = self.cls4d(flat4d).unsqueeze(2)
-        logit5d = self.cls5d(flat5d).unsqueeze(2)
+    out = F.upsample_bilinear(out, scale_factor=2)  # 128
+    out = out + conv1d
+    out = self.conv1u(out)
+    flat1u = make_max_flat(out)
 
-        logit1u = self.cls1u(flat1u).unsqueeze(2)
-        logit2u = self.cls2u(flat2u).unsqueeze(2)
-        logit3u = self.cls3u(flat3u).unsqueeze(2)
-        logit4u = self.cls4u(flat4u).unsqueeze(2)
+    logit2d = self.cls2d(flat2d).unsqueeze(2)
+    logit3d = self.cls3d(flat3d).unsqueeze(2)
+    logit4d = self.cls4d(flat4d).unsqueeze(2)
+    logit5d = self.cls5d(flat5d).unsqueeze(2)
 
-        logit = torch.cat((
-            logit2d, logit3d, logit4d, logit5d,
-            logit1u, logit2u, logit3u, logit4u,
-        ), dim=2)
+    logit1u = self.cls1u(flat1u).unsqueeze(2)
+    logit2u = self.cls2u(flat2u).unsqueeze(2)
+    logit3u = self.cls3u(flat3u).unsqueeze(2)
+    logit4u = self.cls4u(flat4u).unsqueeze(2)
 
-        logit = F.dropout(logit, p=0.15, training=self.training)
-        logit = logit.sum(2)
-        logit = logit.view(logit.size(0), logit.size(1))  # unsqueeze(2)
-        prob = F.sigmoid(logit)
+    logit = torch.cat((
+        logit2d, logit3d, logit4d, logit5d,
+        logit1u, logit2u, logit3u, logit4u,
+    ), dim=2)
 
-        return logit
+    logit = F.dropout(logit, p=0.15, training=self.training)
+    logit = logit.sum(2)
+    logit = logit.view(logit.size(0), logit.size(1))  # unsqueeze(2)
+    prob = F.sigmoid(logit)
 
-
-#############################################################################3
+    return logit  #############################################################################3
 
 
 class PyNet_11(nn.Module):
