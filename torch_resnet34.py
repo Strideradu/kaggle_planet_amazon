173e3d031aeac890e988c759a1d3b07115d33992
Nan Du <dunan00001@gmail.com>
2017-06-27 22:16:09 -0400

    redo resnet 34

--- torch_resnet34.py
+++ torch_resnet34.py
@@ -312,7 +312,7 @@ for f, tags in test.values[:]:
     img_path = test_path + "{}.jpg".format(f)
     img = Image.open(img_path)
     img = img.convert('RGB')
-    x = test_transform(img)
+    x = input_transform_augmentation(img)
     # x = np.expand_dims(x, axis=0)
     X_test.append(x)
 
