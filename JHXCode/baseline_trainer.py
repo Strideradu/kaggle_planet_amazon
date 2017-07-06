005af9f23c5a16d3f5a750478a9e47fe7b388c80
Strideradu <dunan00001@hotmail.com>
2017-07-06 16:21:04 -0400

    specify log position

--- JHXCode/baseline_trainer.py
+++ JHXCode/baseline_trainer.py
@@ -122,7 +122,7 @@ def train_baselines():
         name = str(model).split()[1]
         print('*****Start Training {} with batch size {}******'.format(name, batch))
         print(' epoch   iter   rate  |  smooth_loss   |  train_loss  (acc)  |  valid_loss  (acc)  | total_train_loss\n')
-        logger = Logger('../log/full_data_{}'.format(name), name)
+        logger = Logger('/mnt/home/dunan/Learn/Kaggle/planet_amazon/log/full_data_{}'.format(name), name)
 
         # load pre-trained model on train-37479
         net = model(pretrained=True)
